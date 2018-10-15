0fa98c87952ef46c9bcc70b05377030aa5fa3661
Ruben <lukesruben@gmail.com>
2018-10-15 12:48:50 +0200

    subscribing to observable with observers

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import { Observable, of, from, fromEvent, concat } from 'rxjs';
 import { allBooks, allReaders } from './data';
 
+//#region creating observables
+
 /* function subscribe(subscriber) {
     for (let book of allBooks) {
         subscriber.next(book);
@@ -30,7 +32,7 @@ concat(source1$, source2$)
         console.log(value);
     }) */
 
-
+/*
 let button = document.getElementById('readersButtom');
 
 fromEvent(button, 'click')
@@ -41,5 +43,27 @@ fromEvent(button, 'click')
         for (let reader of allReaders) {
             readersDiv.innerHTML += reader.name + "</br>";
         }
+    });*/
+//#endregion
 
-    });
+//#region Subscribing to Observables with observers
+
+let books$ = from(allBooks);
+
+//with object literal
+/* let booksObserver = {
+    next: book => console.log(`Title:  ${book.title}`),
+    error: err => console.log(`Error: ${err}`),
+    complete: () => console.log("completed!")
+};
+books$.subscribe(booksObserver); */
+//-----------------------------------------------------//
+
+//with call back
+books$.subscribe(
+    //behind the scenes, rxjs is goging to turn these tre functions  into an observer object
+    book => console.log(`Title:  ${book.title}`),
+    err => console.log(`Error: ${err}`),
+    () => console.log("completed!")
+);
+    //#endregion
